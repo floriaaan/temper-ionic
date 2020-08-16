5f905df947b6598f869d1e4a0f62e91f8abcf682
Florian Leroux <florian.leroux3@laposte.net>
2020-08-16 22:42:52 +0200

    test: changes localstorage handling

--- src/pages/settings/DevTools.tsx
+++ src/pages/settings/DevTools.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   IonPage,
   IonContent,
@@ -8,14 +8,37 @@ import {
   IonLabel,
   IonItemOptions,
   IonItemOption,
+  IonToggle,
 } from "@ionic/react";
 import Header from "../../components/Layout/Header";
 
 const DevTools: React.FC = () => {
+  let dev_json = JSON.parse(
+    sessionStorage.getItem("dev") || `{"toggle1": "false"}`
+  );
+
+  const [dev1, setDev1] = useState(dev_json.toggle1);
+  const handleDev1 = (checked: boolean) => {
+    setDev1(checked);
+    dev_json.toggle1 = checked;
+    sessionStorage.setItem("dev", JSON.stringify(dev_json));
+  };
+
   return (
     <IonPage>
       <IonContent>
         <Header title="Dev Tools" version={2.1}></Header>
+
+        <IonList>
+          <IonItem>
+            <IonLabel>Toggle 1</IonLabel>
+            <IonToggle
+              checked={dev1}
+              onIonChange={(e) => handleDev1(e.detail.checked)}
+              slot="end"
+            ></IonToggle>
+          </IonItem>
+        </IonList>
       </IonContent>
     </IonPage>
   );
